04585e7c7aa71ad61989efd6a61ea0385738ed70
Bella Udtohan <blancamystica11@gmail.com>
2025-09-29 23:54:40 +0800

    Input OutLine Graphic Code in Placement Code
    The Dev doesn't know what GameObject to put the Placement Code
    
    Please Update this Dev later
    
    ps. hehehe ka professional ba sa message

--- Assets/Placement.cs
+++ Assets/Placement.cs
@@ -19,6 +19,13 @@ public class Placement : MonoBehaviour
     public RectTransform Buttons;
     public GameObject ButtonsUI;
 
+    [Header(" Line Thingy")]
+    public Material defaultMaterial;
+    public Material outlineMaterial;
+    private Renderer objectRenderer;
+    private bool isHovered = false;
+
+
 
     private void Awake()
     {
@@ -28,20 +35,62 @@ public class Placement : MonoBehaviour
         isInteracted = false;
         timeHold = 0;
 
+        //Line
+
+        objectRenderer = GetComponent<Renderer>();
+        objectRenderer.material = defaultMaterial;
+
     }
 
     private void Update()
     {
         Vector3 rotates = cameraLoc.position - transform.position;
-        Quaternion lookR = Quaternion.LookRotation(rotates*-1);
+        Quaternion lookR = Quaternion.LookRotation(rotates * -1);
         Vector3 rotateTrack = Quaternion.Lerp(Buttons.rotation, lookR, Time.deltaTime * 10).eulerAngles;
         Buttons.rotation = Quaternion.Euler(rotateTrack.x, rotateTrack.y, rotateTrack.z);
 
-        if (isInteracted && Input.GetKeyDown(KeyCode.Backspace)) {
+        if (isInteracted && Input.GetKeyDown(KeyCode.Backspace))
+        {
             camManager.switchCam(camManager.mainCam);
-            isInteracted=false;
+            isInteracted = false;
             ButtonsUI.SetActive(false);
         }
+
+        //Line maybe force update?
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100f, placementMask))
+        {
+            if (hit.transform == transform)
+            {
+                if (!isHovered)
+                {
+                    objectRenderer.material = outlineMaterial;
+                    isHovered = true;
+                }
+
+                // Optional: detect hold
+                if (Input.GetMouseButton(0)) // left click held
+                {
+                    timeHold += Time.deltaTime;
+                    // You can trigger additional effects here
+                }
+            }
+            else if (isHovered)
+            {
+                objectRenderer.material = defaultMaterial;
+                isHovered = false;
+                timeHold = 0;
+            }
+        }
+        else if (isHovered)
+        {
+            objectRenderer.material = defaultMaterial;
+            isHovered = false;
+            timeHold = 0;
+        }
+
     }
 
     public void MovePlacement() {
